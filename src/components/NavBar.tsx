59131b287a97b6e1f25e3ca3087d84bc2cfaa614
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-11-26 17:40:57 +0200

    path

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 export const NavBar: React.FC = () => (
   <nav>
     <div className="nav-wrapper purple darken-2 px1">
-      <NavLink to="/" className="brand-logo">React + TypeScript</NavLink>
+      <NavLink to="/react-typescript" className="brand-logo">React + TypeScript</NavLink>
       <ul className="right hide-on-med-and-down">
         <li>
-          <NavLink to="/">
+          <NavLink to="/react-typescript">
             List of todos
           </NavLink>
         </li>
